7d42b701d636750de7b9651cfadf25f094f63fce
zhuyijing <zhuyijing168@163.com>
2021-09-07 17:36:45 +0800

    dev

--- views/settings.js
+++ views/settings.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { Avatar } from 'react-native-elements';
 
 function SettingsScreen() {
     return (
@@ -8,6 +9,21 @@ function SettingsScreen() {
             <LinearGradient
                 colors={['#157DEC', '#3090C7', '#79BAEC']}
                 style={[{ backgroundColor: '#82CAFF', flex: 1 }, styles.card, styles.shadowProp]}>
+                <View style={[{ flex: 1 }, styles.alignBothCenter]}>
+                    <Avatar
+                        rounded
+                        size="large"
+                        source={require("../assets/images/logo.png")}
+                    />
+                </View>
+                <View style={[{ flex: 1 }, styles.alignBothCenter]}>
+                    <View style={[{ flex: 1 }, styles.alignBothCenter]}>
+                        <Text style={{ fontSize: 20, color: 'white' }}>React Native Starter v1.0.1</Text>
+                        <Text style={{ fontSize: 13, color: 'white' }}>By: 皓月当空</Text>
+                    </View>
+                    <View style={{ flex: 1 }}>
+                    </View>
+                </View>
             </LinearGradient>
             <View style={[styles.card, { backgroundColor: '#FEFCFF', flex: 2 }, styles.shadowProp]}>
             </View>
@@ -28,6 +44,10 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.2,
         shadowRadius: 3,
     },
+    alignBothCenter: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
 })
 
 export { SettingsScreen };
