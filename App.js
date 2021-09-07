38d0f6fd455848ab5cd3a9a63d24f3265c1ed77a
zhuyijing <zhuyijing168@163.com>
2021-09-07 11:16:43 +0800

    dev

--- App.js
+++ App.js
@@ -4,44 +4,32 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
+import { HomeScreen } from './views/home';
+import { SettingsScreen } from './views/settings';
 
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
 
 const Tab = createBottomTabNavigator();
 
 function MyTabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen 
-      options={{
-        tabBarLabel: "主页",
-        tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons name="home" color={color} size={size} />
-        ),
-        tabBarBadge: 3,
-      }}
-      name="Home" component={HomeScreen} />
-      <Tab.Screen 
-      options={{
-        tabBarLabel: "关于",
-        tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons name="information" color={color} size={size} />
-        ),
-      }}
-      name="Settings" component={SettingsScreen} />
+      <Tab.Screen
+        options={{
+          tabBarLabel: "主页",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+          tabBarBadge: 3,
+        }}
+        name="Home" component={HomeScreen} />
+      <Tab.Screen
+        options={{
+          tabBarLabel: "关于",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="information" color={color} size={size} />
+          ),
+        }}
+        name="Settings" component={SettingsScreen} />
     </Tab.Navigator>
   );
 }
