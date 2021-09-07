07b2245c9b5302ea03580ba5dd0f177d3e5d6ee4
zhuyijing <zhuyijing168@163.com>
2021-09-07 17:05:14 +0800

    dev

--- views/settings.js
+++ views/settings.js
@@ -1,12 +1,33 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 function SettingsScreen() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Settings!</Text>
-      </View>
+        <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center', backgroundColor: '#F0F8FF' }}>
+            <LinearGradient
+                colors={['#157DEC', '#3090C7', '#79BAEC']}
+                style={[{ backgroundColor: '#82CAFF', flex: 1 }, styles.card, styles.shadowProp]}>
+            </LinearGradient>
+            <View style={[styles.card, { backgroundColor: '#FEFCFF', flex: 2 }, styles.shadowProp]}>
+            </View>
+        </View>
     );
-  }
+}
+
+const styles = StyleSheet.create({
+    card: {
+        margin: 5,
+        borderRadius: 8,
+        height: 200,
+        width: '96%',
+    },
+    shadowProp: {
+        shadowColor: 'black',
+        shadowOffset: { width: 3, height: 3 },
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
+    },
+})
 
 export { SettingsScreen };
